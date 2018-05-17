b178d9d0187cc9158ac92fa34ae8d173968b5ba8
Tumble <tumblegamer@gmail.com>
2018-05-17 17:50:43 +0100

    last data

--- githubSave.js
+++ githubSave.js
@@ -1,7 +1,7 @@
 const Github = require('github-api');
 const path = require('path');
 
-var lastData = [];
+var lastData = "";
 
 var github = new Github({
   token: process.env.OAUTH_TOKEN || require('./config/githubOauth.js').token,
@@ -11,6 +11,10 @@ var github = new Github({
 
 var repo = github.getRepo("tumble1999", "discordPhoneBot");
 
+repo.read('master', 'data/phones.json', function (data) {
+    lastData = data;
+  })
+
 var options = {
   author: {name: 'HerokuSave', email: 'api@tumblenet.ga'},
   committer: {name: 'TN Phones', email: 'admin@tumblenet.ga'},
@@ -18,13 +22,15 @@ var options = {
 }
 
 function SavePhones(data, cb) {
+  repo.read('master', 'data/phones.json', function (err,data) {
+    lastData = data;
+  });
   var callback = cb || function (err) {
     console.log(err);
   }
-  if (JSON.stringify(lastData, null, '\t') == JSON.stringify(data, null, '\t')) {
+  if (lastData == JSON.stringify(data, null, '\t')) {
     callback("phones is already up to date")
   } else {
-    lastData = data;
     repo.writeFile('master', 'data/phones.json', JSON.stringify(data, null, '\t'), 'Update Phones feed', options,
     function(error,request,result) {
       callback(error ? "There was an error in updateing phones data" : "phones data was updated");
