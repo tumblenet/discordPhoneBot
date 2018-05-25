35d8a92a86f7189fe5a5c8773f0ae7bff9ede18c
Tumble <tumblegamer@gmail.com>
2018-05-25 17:19:13 +0100

    author

--- discordbot.js
+++ discordbot.js
@@ -219,7 +219,7 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-  if (message.member.bot) {
+  if (message.author.bot) {
     return;
   }
   if (message.type != "DEFAULT") {
