b20e2cc217de688315ce934ee4fbb96eab2a1597
Tumble <tumblegamer@gmail.com>
2018-10-11 19:24:34 +0100

    fixed start error

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const discordBot = require('./discordBot.js');
 
-var token = process.env.DISCORD_TOKEN || require("./config/token.js".token);
+var token = process.env.DISCORD_TOKEN || require("./config/token.js").token;
 
 discordBot.login(token);
