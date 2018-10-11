dec57196e8aa89c6f9119e6f570b70667ff20e41
Tumble <tumblegamer@gmail.com>
2018-10-11 20:35:44 +0100

    basic commands

--- discordBot.js
+++ discordBot.js
@@ -1,9 +1,27 @@
 const Discord = require('discord.js');
 const Commando = require('discord.js-commando');
+const path = require('path');
 
 const client = new Commando.Client({
-  owner:'336869008148135948'
-})
+  commandPrefix: '=T:',
+  owner:'336869008148135948',
+  invite: 'https://discord.gg/TWbkwT9'
+});
+
+client.registry
+    // Registers your custom command groups
+    registerGroups([
+        ['phone', 'Phone Commands'],
+        ['wwtbam', 'Who wants to be a millionare'],
+        ['settings', 'Settings'],
+        ['info', 'Information and Statistics']
+    ])
+
+    // Registers all built-in groups, commands, and argument types
+    .registerDefaults()
+
+    // Registers all of your commands in the ./commands/ directory
+    .registerCommandsIn(path.join(__dirname, 'commands'));
 
 client.on('ready',()=> {
   console.log(`Logged in as ${client.user.tag}`);
