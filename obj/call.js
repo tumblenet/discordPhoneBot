44b17fd97f24cbf21ea11e6d59ec32c0c5951be3
Tumble <tumblegamer@gmail.com>
2018-10-14 16:01:48 +0100

    jar doc

--- obj/call.js
+++ obj/call.js
@@ -1,4 +1,17 @@
+/**
+ * Class rep56resenting a caller45678i9op[]
+ asdcfvbnm,./
+ * @typedef Call
+ * @type {Object}
+ * @property {string} id
+ * @property {Phone[]} members
+ */
 class Call {
+  /**
+   * Create a new phone
+   * @param {string} id - Call id for reference when joing directly
+   * @param {Phone} phone - The fist phone to join the call
+   */
   constructor(id,phone) {
     this.id = id;
     this.members = [];
@@ -7,12 +20,20 @@ class Call {
     console.log("Call Created: (" + this.members.length + ") members");
   }
 
+  /**
+   * Join a phone to a Call
+   * @param {Phone}
+   */
   join(phone) {
     this.members.push(phone);
     phone.inCall = true;
     console.log("Caller (" + phone.name + ") Joined Call (" + this.getName() + ")");
   }
 
+  /**
+   * Disconect a phone from a Call
+   * @param {Phone}
+   */
   leave(phone) {
     var index = this.members.indexOf(phone);
     if (index > -1) {
@@ -21,10 +42,19 @@ class Call {
     phone.inCall = false;
   }
 
+  /**
+   * Gets the name of a Call
+   * @return {string} Name of the call
+   */
   getName() {
     return "Call (" + this.members.length + ") members - [" + this.getMembers() + "]";
   }
 
+  /**
+   * Gets the members in a Call starting from specified id
+   * @param {string} id - Id of phone to start from
+   * @return {string} Member list
+   */
   getMembers(id=0) {
     if (id == this.members.length || id == 5) {
       return "";
